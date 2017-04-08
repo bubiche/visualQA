9899fad9f47faf00986239c156a70cf8063d5b9c
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-04-08 13:44:50 +0700

    Bao's insight

--- parser/read_json.py
+++ parser/read_json.py
@@ -41,8 +41,7 @@ def extract_images(cap, n, directory, step, skip, verbose = True):
         i += step
 
 def vid_vec_from_dir(directory):
-    img_list = [f for f in os.listdir(directory) if f.endswith('.bmp')]
-    img_list = list(map(lambda x: '%s%s' % (BASE_VID_PATH, x), img_list))
+    img_list = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.bmp')]
     print('Frame count: %d' % (len(img_list)))
     print(img_list)
     vec = yolo_net.forward(img_list)
