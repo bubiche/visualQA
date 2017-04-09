15b6481f84ef9647bd09584ad1018443a030ea74
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-04-09 22:12:28 +0700

    add main

---
+++ main.py
@@ -0,0 +1,24 @@
+from horse.net import HorseNet
+from tensorflow import flags
+import os
+
+flags.DEFINE_integer("batch_size", 128, "size of each batch")
+flags.DEFINE_integer("epoch", 1000, "number of epoch")
+flags.DEFINE_string("vec_path", 'parser/full_vec.hdf5', "path of vec file")
+flags.DEFINE_string("count_path", 'parser/full_count.hdf5', "path of count file")
+flags.DEFINE_integer("n_use", -1, "number of training example to be used")
+flags.DEFINE_boolean("train", True, "train the whole net")
+flags.DEFINE_float("lr", 1e-5, "learning rate")
+flags.DEFINE_string("trainer", "rmsprop", "training algorithm")
+flags.DEFINE_boolean("savepb", False, "save net and weight to a .pb file")
+FLAGS = flags.FLAGS
+
+horse_net = HorseNet(FLAGS)
+
+if FLAGS.train:
+    print('Enter training ...'); horse_net.train()
+    if not FLAGS.savepb: exit('Training finished')
+    
+flags.DEFINE_boolean("savepb", False, "save net and weight to a .pb file")
+
+horse_net.predict()
