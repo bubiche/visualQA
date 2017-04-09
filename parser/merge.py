860ee8aa4e701b77fc3cc9ba9804d312755f501a
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-04-09 17:51:15 +0700

    add merger

---
+++ parser/merge.py
@@ -0,0 +1,31 @@
+import h5py
+
+def merge_dat(f1, f2, dset_name, out_file):
+    input_file_1 = h5py.File(f1, 'r')
+    input_file_2 = h5py.File(f2, 'r')
+    output_file = h5py.File(out_file, 'w')
+    
+    dset1 = input_file_1[dset_name]
+    dset2 = input_file_2[dset_name]
+    
+    total_size = dset1.shape[0] + dset2.shape[0]
+    
+    if dset_name == 'vec':
+        out_dset = output_file.create_dataset(dset_name, (total_size, 14, 14, 512), dtype='f')
+    elif dset_name == 'count':
+        out_dset = output_file.create_dataset(dset_name, (total_size,), dtype='i')
+    else:
+        return
+    
+    i = 0
+    for dat in dset1:
+        out_dset[i] = dat
+        i += 1
+        
+    for dat in dset2:
+        out_dset[i] = dat
+        i += 1
+        
+    output_file.close()
+    input_file_1.close()
+    input_file_2.close()
