b367167726fba786e321cffcd154625bf8e9288a
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-04-10 23:10:57 +0700

    parse voc upto 23

--- parser/parsevoc.py
+++ parser/parsevoc.py
@@ -13,16 +13,16 @@ sorted_data = sorted(data.items(), key=operator.itemgetter(1), reverse=True)
 data_size = len(sorted_data)
 
 print('create hdf5 file')
-img_vec_file = h5py.File('voc_vec.hdf5', 'w')
-count_file = h5py.File('voc_count.hdf5', 'w')
-img_dset = img_vec_file.create_dataset('vec', (data_size, 7, 7, 1024), dtype='f')
+img_vec_file = h5py.File('voc_vec_23.hdf5', 'w')
+count_file = h5py.File('voc_count_23.hdf5', 'w')
+img_dset = img_vec_file.create_dataset('vec', (data_size, 14, 14, 512), dtype='f')
 count_dset = count_file.create_dataset('count', (data_size,), dtype='i')
 
 print('Load YOLO...')
 net = yolo.YOLO(
     'image2vec/yolo-full.cfg', 
     'image2vec/yolo-full.weights',
-    up_to = 28)
+    up_to = 23)
 
 i = 0
 for (key, value) in sorted_data:
