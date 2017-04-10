1b08b81bce9885c4c376f12b6ee2b38f0f9c1a52
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-04-10 12:38:13 +0700

    add split data

---
+++ parser/split_data.py
@@ -0,0 +1,73 @@
+class a(object):
+
+    def __init__(self):
+        self.vec_file = h5py.File('full_vec.hdf5', 'r')
+        self.count_file = h5py.File('full_count.hdf5', 'r')
+        self.vec_dset = self.vec_file['vec']
+        self.count_dset = self.count_file['count']
+        self.data_size = self.get_data_size()
+        self.n_use = -1
+        if self.n_use < 1 or self.n_use > self.data_size: self.n_use = self.data_size
+        self.n_val = int(self.n_use * 15 / 100)
+        self.n_test = int(self.n_use * 15 / 100)
+        self.train_size = self.n_use - self.n_val - self.n_test
+        self.shuffle_data()
+        self.create_val_test_files()
+        
+    def create_val_test_files(self):
+        self.val_vec = h5py.File('val_vec.hdf5', 'w')
+        self.val_count = h5py.File('val_count.hdf5', 'w')
+        self.test_vec = h5py.File('test_vec.hdf5', 'w')
+        self.test_count = h5py.File('test_count.hdf5', 'w')
+        self.tran_vec = h5py.File('train_vec.hdf5', 'w')
+        self.tran_count = h5py.File('train_count.hdf5', 'w')
+        
+        self.val_vec_dset = self.val_vec.create_dataset('vec', (self.n_val, 7, 7, 1024), dtype='f')
+        self.val_count_dset = self.val_vec.create_dataset('count', (self.n_val,), dtype='i')
+        self.test_vec_dset = self.test_vec.create_dataset('vec', (self.n_test, 7, 7, 1024), dtype='f')
+        self.test_count_dset = self.test_vec.create_dataset('count', (self.n_test,), dtype='i')
+        self.train_vec_dset = self.train_vec.create_dataset('vec', (self.train_size, 7, 7, 1024), dtype='f')
+        self.train_count_dset = self.train_vec.create_dataset('count', (self.train_size,), dtype='i')
+        
+        print('Dump train')
+        i = 0
+        while i < self.train_size:
+            self.train_vec_dset[i] = self.get_x_at_index(self.shuffle_idx[i])
+            self.train_count_dset[i] = self.get_annotation_at_index(self.shuffle_idx[i])
+            i += 1
+        
+        print('Dump val')
+        i = 0
+        while i < self.n_val:
+            self.val_vec_dset[i] = self.get_x_at_index(self.shuffle_idx[i + self.train_size])
+            self.val_count_dset[i] = self.get_annotation_at_index(self.shuffle_idx[i + self.train_size])
+            i += 1
+          
+        print('Dump test')
+        i = 0
+        while i < self.n_test:
+            self.test_vec_dset[i] = self.get_x_at_index(self.shuffle_idx[i + self.train_size + self.n_val])
+            self.test_count_dset[i] = self.get_annotation_at_index(self.shuffle_idx[i + self.train_size + self.n_val])
+            i += 1
+            
+        self.val_vec.close()
+        self.val_count.close()
+        self.test_vec.close()
+        self.test_count.close()
+        self.tran_vec.close()
+        self.tran_count.close()
+        
+    def get_data_size(self):
+        return self.vec_dset.shape[0]
+
+    def shuffle_data(self):
+        self.shuffle_idx = np.random.permutation(self.data_size)
+        
+    def shuffle_train(self):
+        self.shuffle_idx_train = np.random.permutation(self.train_size)
+
+    def get_x_at_index(self, idx):
+        return self.vec_dset[idx]
+
+    def get_annotation_at_index(self, idx):
+        return self.count_dset[idx]
