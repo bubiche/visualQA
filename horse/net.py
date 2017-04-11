581d57a305908aa07794ab4b97ce4300bf5177fa
thtrieu <thtrieu@apcs.vn>
2017-04-11 15:24:26 +0700

    deep

--- horse/net.py
+++ horse/net.py
@@ -58,17 +58,17 @@ class HorseNet(object):
 		similar = (similar + 1.) / 2.
 
 		sharped = sharpen(similar)
-		self._out = tf.reduce_sum(sharped, -1)
+		# self._out = tf.reduce_sum(sharped, -1)
 		#self._fetches += [self._yolo._inp]
-		# attention = tf.reshape(sharped, [-1, 7, 7, 1])
-		# focused = self._volume * attention
+		attention = tf.reshape(sharped, [-1, 7, 7, 1])
+		focused = self._volume * attention
 
-		# conved = conv_pool_leak(focused, 1024, 512)
-		# feat = tf.reduce_sum(conved, [1, 2])
+		conved = conv_pool_leak(focused, 1024, 512)
+		feat = tf.reduce_sum(conved, [1, 2])
 
-		# feat = tf.matmul(feat, xavier_var('fcw', [512, 1]))
-		# feat += const_var('fcb', 0.0, [1,])
-		# self._out = tf.nn.softplus(feat)
+		feat = tf.matmul(feat, xavier_var('fcw', [512, 1]))
+		feat += const_var('fcb', 0.0, [1,])
+		self._out = tf.nn.softplus(feat)
 
 		if self._flags.train:
 			self._build_loss()
