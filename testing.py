2bbff1a9d6840e3ea20f84c665c01eaaee90e552
thtrieu <thtrieu@apcs.vn>
2017-04-18 16:05:36 +0700

    testing xam

--- testing.py
+++ testing.py
@@ -1,12 +1,2 @@
-from horse.yolo_top import YOLOtop
-from image2vec.yolo import YOLO
-
-net = YOLOtop(
-	'image2vec/yolo-full.cfg', 
-	'image2vec/yolo-full.weights',
-	from_layer = 28)
-
-# net = YOLO(
-# 	'image2vec/yolo-full.cfg', 
-# 	'image2vec/yolo-full.weights',
-# 	up_to = 100)
+with open('log', 'a') as f:
+	f.write('lsajfldsf\n')
