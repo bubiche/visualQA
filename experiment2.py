c3da39602740d43c0ed137ea77bf0e8812d5b752
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-04-19 17:42:41 +0700

    fix ngu

--- experiment2.py
+++ experiment2.py
@@ -4,7 +4,7 @@ import sys
 conf_list = [(1, 'noref_nosharp'), (2, 'ref_nosharp'), (6, 'ref_power')]
              
 cls_list_1 = ['chair', 'car', 'dog', 'horse', 'sheep', 'cow', 'train']
-cls_list_2 = ['bicycle', 'pottedplant', 'tvmoninor', 'aeroplane', 'boat', 'diningtable', 'bottle']
+cls_list_2 = ['bicycle', 'pottedplant', 'tvmonitor', 'aeroplane', 'boat', 'diningtable', 'bottle']
 
 if sys.argv[1] == 'may1':
     cls_list = cls_list_1
