cd24a12e7a158d7aa7f033926dede7cdb32d6a5a
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-04-22 14:18:08 +0700

    test

--- testing.py
+++ testing.py
@@ -1,2 +1,10 @@
-with open('log', 'a') as f:
-	f.write('lsajfldsf\n')
+import h5py
+
+f = h5py.File('full_split_voc.hdf5', 'r')
+d = f['test']
+idx = d[2451]
+f.close()
+f = h5py.File('full_name_voc.hdf5', 'r')
+d = f ['name']
+fn = d[idx]
+print(fn.decode())
