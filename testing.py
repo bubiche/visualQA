924713af7fbaceedaba6fe50733b0bcffa140f5c
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-04-22 14:36:00 +0700

    why

--- testing.py
+++ testing.py
@@ -1,8 +1,10 @@
 import h5py
+import sys
 
+img_id = int(sys.argv[1])
 f = h5py.File('parser/full_split_voc.hdf5', 'r')
 d = f['test']
-idx = d[2451]
+idx = d[img_id]
 f.close()
 f = h5py.File('parser/full_name_voc.hdf5', 'r')
 d = f ['name']
