c8cbf954791e215e46a1e78e9e436275f69bf1e9
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-04-22 14:19:17 +0700

    test

--- testing.py
+++ testing.py
@@ -1,10 +1,10 @@
 import h5py
 
-f = h5py.File('full_split_voc.hdf5', 'r')
+f = h5py.File('parser/full_split_voc.hdf5', 'r')
 d = f['test']
 idx = d[2451]
 f.close()
-f = h5py.File('full_name_voc.hdf5', 'r')
+f = h5py.File('parser/full_name_voc.hdf5', 'r')
 d = f ['name']
 fn = d[idx]
 print(fn.decode())
