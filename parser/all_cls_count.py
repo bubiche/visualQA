b97128c9dae8c558abb055a966bd02e6962a3861
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-04-23 14:36:55 +0700

    conspiracy

--- parser/all_cls_count.py
+++ parser/all_cls_count.py
@@ -3,41 +3,46 @@ import h5py
 count_file = h5py.File('full_count_voc.hdf5', 'a')
 
 check = {}
-check['aeroplane'] = 0
-check['bicycle'] = 1
-check['bird'] = 2
-check['boat'] = 3
-check['bottle'] = 4
-check['bus'] = 5
-check['car'] = 6
-check['cat'] = 7
-check['chair'] = 8
-check['cow'] = 9
-check['diningtable'] = 10
-check['dog'] = 11
-check['horse'] = 12
-check['motorbike'] = 13
-check['person'] = 14
-check['pottedplant'] = 15
-check['sheep'] = 16
-check['sofa'] = 17
-check['train'] = 18
-check['tvmonitor'] = 19
+check['aeroplane'] = 2
+check['bicycle'] = 2
+check['boat'] = 2
+check['bottle'] = 1
+check['car'] = 2
+check['chair'] = 1
+check['cow'] = 0
+check['diningtable'] = 1
+check['dog'] = 0
+check['horse'] = 0
+check['pottedplant'] = 1
+check['sheep'] = 0
+check['train'] = 2
+check['tvmonitor'] = 1
 
 count_dset = []
 
 for key, value in check.items():
     count_dset.append(count_file[key])
-    
-all_count_dset = count_file.create_dataset('all', (27088,), dtype='i')
+
+animal_count_dset = count_file.create_dataset('animal', (27088,), dtype='i')
+furniture_count_dset = count_file.create_dataset('furniture', (27088,), dtype='i')
+vehicle_count_dset = count_file.create_dataset('vehicle', (27088,), dtype='i')
 
 for i in range(27088):
-    tmp = 0
-    for j in range(20):
-        tmp += count_dset[j][i]
+    count_animal = 0
+    count_furniture = 0
+    count_vehicle = 0
+
+    for key, value in check.items():
+        if value == 0:
+            count_animal += count_file[key][i]
+        elif value == 1:
+            count_furniture += count_file[key][i]
+        elif value == 2:
+            count_vehicle += count_file[key][i]
+            
+        animal_count_dset[i] = count_animal
+        furniture_count_dset[i] =  count_furniture
+        vehicle_count_dset[i] = count_vehicle
         
-    all_count_dset[i] = tmp
-    print(all_count_dset[i])
-    
 count_file.close()
 
