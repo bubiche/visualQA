31085efe1578d204366fc26061fe337c326bb4d2
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-04-24 16:56:37 +0700

    no no

--- visualize_cls_khung.py
+++ visualize_cls_khung.py
@@ -5,7 +5,7 @@ animal_A_list = [2041, 3998, 4013, 3764, 9, 2955, 4013, 3673, 3818, 3558, 3023, 
 vehicle_A_list = [999, 2480, 640, 1392, 3317, 3019, 3917, 3800, 3141, 2570, 3777, 3583]
 indoor_A_list = [3308, 3805, 3739, 3668, 3464, 2971, 2392, 3118, 3308, 3478, 2834, 3943]
 
-conf_list_1 = [(0, 'no_attention'), (1, 'noref_nosharp'), (2, 'ref_nosharp'), (3, 'noref_softmax')]
+conf_list_1 = [(1, 'noref_nosharp'), (2, 'ref_nosharp'), (3, 'noref_softmax')]
 conf_list_2 = [(4, 'ref_softmax'), (5, 'noref_power'), (6, 'ref_power')]
 if sys.argv[1] == 'may1':
     conf_list = conf_list_1
