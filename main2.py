8377dac7556848defea1bae11dd9df9100f326ae
thtrieu <thtrieu@apcs.vn>
2017-04-26 17:47:45 +0700

    main2

---
+++ main2.py
@@ -0,0 +1,21 @@
+from tensorflow import flags
+from image2vec.yolo import YOLO
+import os
+
+flags.DEFINE_integer('epoch', 100, 'no. epoch')
+flags.DEFINE_string('cfg', 
+	'./parser/image2vec/yolo-full.cfg', 'where is the config')
+flags.DEFINE_string('wgt', 
+	'./parser/image2vec/yolo-full.weights', 'where is the weights')
+flags.DEFINE_integer('up_to', 28, 'up to layer')
+flags.DEFINE_float('lr', 1e-3, 'learning rate')
+flags.DEFINE_string('target', 'whatever', 'just a placeholder')
+FLAGS = flags.FLAGS
+
+
+all_targets = os.listdir('trained_refs')
+for target in all_targets:
+	FLAGS.target = target
+	tf.reset_default_graph()
+	net = YOLO(FLAGS)
+	
