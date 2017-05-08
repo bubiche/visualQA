64fbeb88d6b67e76756e117e28626ac63204e357
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-05-08 22:55:50 +0700

    yay

--- exp11.py
+++ exp11.py
@@ -3,8 +3,8 @@ import sys
 
 conf_list = [(1, 'noref_nosharp'), (2, 'ref_nosharp'), (6, 'ref_power')]
              
-cls_list_1 = ['chair', 'car', 'dog', 'horse', 'sheep', 'cow', 'train']
-cls_list_2 = ['bicycle', 'pottedplant', 'tvmonitor', 'aeroplane', 'boat', 'diningtable', 'bottle']
+cls_list_1 = ['animal', 'vehicle']
+cls_list_2 = ['furniture']
 
 if sys.argv[1] == 'may1':
     cls_list = cls_list_1
