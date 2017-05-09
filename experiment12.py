0def87fc29d2b14c5b7e3ceb918fd957b887661f
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-05-09 21:02:58 +0700

    ielts

--- experiment12.py
+++ experiment12.py
@@ -4,6 +4,7 @@ import sys
 for filename in os.listdir('videos'):
     if filename.endswith('.mp4'):
         vid_path = os.path.join('videos', filename)
-        out_path = os.path.join('out_vid', filename)
-        my_cmd = '/home/tmbao_1995/miniconda3/bin/python main.py --load=620 --cls=person --config=2 --see_vid="{}" --output_file_vid="{}"'.format(vid_path, filename)
+        out_path = os.path.join('out_vid', filename.replace('.mp4', '.avi'))
+        
+        my_cmd = '/home/tmbao_1995/miniconda3/bin/python main.py --load=620 --cls=person --config=2 --see_vid="{}" --output_file_vid="{}"'.format(vid_path, out_path)
         os.system(my_cmd)
