d0b41231c0fc0eb47a859e7dfc334ea2d4db4789
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-05-09 20:55:25 +0700

    ln

--- experiment12.py
+++ experiment12.py
@@ -5,5 +5,5 @@ for filename in os.listdir('videos'):
     if filename.endswith('.mp4'):
         vid_path = os.path.join('videos', filename)
         out_path = os.path.join('out_vid', filename)
-        my_cmd = '/home/tmbao_1995/miniconda3/bin/python main.py --load=620 --cls=person --config=2 --see_vid={} --output_file_vid={}'.format(vid_path, filename)
+        my_cmd = '/home/tmbao_1995/miniconda3/bin/python main.py --load=620 --cls=person --config=2 --see_vid="{}" --output_file_vid="{}"'.format(vid_path, filename)
         os.system(my_cmd)
