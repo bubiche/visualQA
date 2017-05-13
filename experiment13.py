b3793caf6fe6cdb4e5405dd61e1b6fe0c26cd8ad
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-05-13 18:29:40 +0700

    bird

--- experiment13.py
+++ experiment13.py
@@ -6,6 +6,6 @@ for filename in os.listdir('catbirdvid'):
         vid_path = os.path.join('catbirdvid', filename)
         out_path = os.path.join('out_vid', filename.replace('.mp4', '.avi'))
         
-        cls_name = 'cat'
+        cls_name = 'bird'
         my_cmd = '/home/tmbao_1995/miniconda3/bin/python main.py --load=620 --cls={} --config=2 --see_vid="{}" --output_file_vid="{}"'.format(cls_name, vid_path, out_path)
         os.system(my_cmd)
