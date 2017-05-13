70f7ecd30ab43f575b6020f71cd7c52dd534c6b7
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-05-13 17:43:14 +0700

    dog

--- experiment13.py
+++ experiment13.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-for filename in os.listdir('catvid'):
+for filename in os.listdir('dogvid'):
     if filename.endswith('.mp4'):
-        vid_path = os.path.join('catvid', filename)
+        vid_path = os.path.join('dogvid', filename)
         out_path = os.path.join('out_vid', filename.replace('.mp4', '.avi'))
         
-        cls_name = 'cat'
+        cls_name = 'dog'
         my_cmd = '/home/tmbao_1995/miniconda3/bin/python main.py --load=620 --cls={} --config=2 --see_vid="{}" --output_file_vid="{}"'.format(cls_name, vid_path, out_path)
         os.system(my_cmd)
