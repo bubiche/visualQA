cabbf3bb94d18177d05d7ce5a4cd0fcf8c70e3be
Nguyen Phuc Nguyen <npnguyen@apcs.vn>
2017-05-13 17:13:05 +0700

    osb

--- experiment13.py
+++ experiment13.py
@@ -7,5 +7,5 @@ for filename in os.listdir('catbirdvid'):
         out_path = os.path.join('out_vid', filename.replace('.mp4', '.avi'))
         
         cls_name = 'animal_A' 
-        my_cmd = '/home/tmbao_1995/miniconda3/bin/python main.py --load=620 --cls={} --config=2 --see_vid="{}" --output_file_vid="{}"'.format(cls_name, vid_path, out_path)
+        my_cmd = '/home/tmbao_1995/miniconda3/bin/python main.py --load=413 --cls={} --config=2 --see_vid="{}" --output_file_vid="{}"'.format(cls_name, vid_path, out_path)
         os.system(my_cmd)
